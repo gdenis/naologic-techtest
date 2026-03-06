3e37ed72c08893548e74309f724ad26fd5e0afdd
Gabriel Denis Llanes <gabydll95@gmail.com>
2026-03-06 15:55:38 -0500

    Material added to project

--- projects/naologic-ds/src/lib/components/naologic-ds/naologic-ds.ts
+++ projects/naologic-ds/src/lib/components/naologic-ds/naologic-ds.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgSelectComponent, NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'nao-welcome',
-  imports: [NgbAlertModule, FormsModule, NgSelectModule, NgSelectComponent],
+  imports: [
+    NgbAlertModule,
+    FormsModule,
+    NgSelectModule,
+    NgSelectComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './naologic-ds.html',
   styleUrl: './naologic-ds.scss',
 })
 export class NaologicDs {
-   cars = [
-        { id: 1, name: 'Volvo' },
-        { id: 2, name: 'Saab' },
-        { id: 3, name: 'Opel' },
-        { id: 4, name: 'Audi' },
-    ];
+  mode = new FormControl('over' as MatDrawerMode);
+
+  cars = [
+    { id: 1, name: 'Volvo' },
+    { id: 2, name: 'Saab' },
+    { id: 3, name: 'Opel' },
+    { id: 4, name: 'Audi' },
+  ];
 
   selectedCar: number | undefined = undefined;
 }
