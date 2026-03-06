447af43a5cf17909e4579eea92b44318b5b03b70
Gabriel Denis Llanes <gabydll95@gmail.com>
2026-03-05 19:08:43 -0500

    Added ng-select to project

--- projects/naologic-ds/src/lib/components/naologic-ds/naologic-ds.ts
+++ projects/naologic-ds/src/lib/components/naologic-ds/naologic-ds.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectComponent, NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'nao-welcome',
-  imports: [NgbAlertModule],
+  imports: [NgbAlertModule, FormsModule, NgSelectModule, NgSelectComponent],
   templateUrl: './naologic-ds.html',
   styleUrl: './naologic-ds.scss',
 })
-export class NaologicDs {}
+export class NaologicDs {
+   cars = [
+        { id: 1, name: 'Volvo' },
+        { id: 2, name: 'Saab' },
+        { id: 3, name: 'Opel' },
+        { id: 4, name: 'Audi' },
+    ];
+
+  selectedCar: number | undefined = undefined;
+}
